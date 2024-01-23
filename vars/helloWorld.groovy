f32f04e5d831ab7be6950e3b779a2be84bc81d18
saya1a <40052644+saya1a@users.noreply.github.com>
2024-01-24 00:44:48 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call(string name, string dayofweek) {
-    sh "echo Hello ${name}. Today is ${dayofweek"
+def call(String name, String dayOfweek) {
+    sh "echo Hello ${name}. Today is ${dayOfweek"
 }
